c27fc3c0b2697d7421062b7b8558c7a51655c761
FilbertKimasa <filbertdan67@gmail.com>
2023-09-19 13:23:20 +0300

    Add logo by using import

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './style.css';
+import logo from './assets/img/movie-safari-logo.png';
+
+const logoContainer = document.querySelector('.logo-container');
+const imgTag = document.createElement('img');
+imgTag.setAttribute('src', logo);
+imgTag.classList.add('logo');
+logoContainer.appendChild(imgTag);
