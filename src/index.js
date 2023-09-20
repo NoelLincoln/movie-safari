bfe85b0e8eadd87a2f5d90f751400377ccae9082
FilbertKimasa <filbertdan67@gmail.com>
2023-09-20 17:01:17 +0300

    Import domCheck and like variables

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
 import logo from './assets/img/movie-safari-logo.png';
 import fetchMovies from './modules/data.js';
+import { domCheck, like } from './modules/movie-likes.js';
 
 const logoContainer = document.querySelector('.logo-container');
 const imgTag = document.createElement('img');
@@ -9,3 +10,7 @@ imgTag.classList.add('logo');
 logoContainer.appendChild(imgTag);
 
 document.addEventListener('DOMContentLoaded', fetchMovies);
+
+domCheck.addEventListener('DOMNodeInserted', () => {
+  like();
+});
