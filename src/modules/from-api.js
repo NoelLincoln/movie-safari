067bb963da1de72087803056833954f44fc1b399
FilbertKimasa <filbertdan67@gmail.com>
2023-09-20 21:01:49 +0300

    Add from-api.js file

---
+++ src/modules/from-api.js
@@ -0,0 +1,6 @@
+const interactionUrl =
+  'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi';
+const appId = 'IR7MovTrVQtBQVyC6UTK';
+const likeUrl = `${interactionUrl}/apps/:${appId}/likes/`;
+
+export default likeUrl;
