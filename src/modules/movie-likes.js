726564cf6adfc9c5c4d69a2523ce685c9ec4fa20
FilbertKimasa <filbertdan67@gmail.com>
2023-09-20 21:01:13 +0300

    Add addLike function

--- src/modules/movie-likes.js
+++ src/modules/movie-likes.js
@@ -1,11 +1,29 @@
+import likeUrl from './from-api.js';
+
 const domCheck = document.querySelector('.movies-counter');
 
+const addLike = async (movieId = 'movieId', id) => {
+  try {
+    const response = await fetch(likeUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ movieId, id }),
+    });
+    console.log(response);
+  } catch {
+    throw new Error();
+  }
+};
 const like = () => {
   const likeBtn = document.querySelectorAll('.heart-btn');
   likeBtn.forEach((element) => {
     element.addEventListener('click', (e) => {
       const { id } = e.target.closest('.movie-container');
-      console.log(id);
+      addLike(parseInt(id, 10));
+
+      //   console.log(typeof parseInt(id, 10));
     });
   });
 };
