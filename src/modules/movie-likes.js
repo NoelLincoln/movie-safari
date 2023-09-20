c2fbed402663df85117f826b81efc39e0e26d119
FilbertKimasa <filbertdan67@gmail.com>
2023-09-20 16:59:10 +0300

    Add movie-likes.js file

---
+++ src/modules/movie-likes.js
@@ -0,0 +1,7 @@
+const domCheck = document.querySelector('.movies-counter');
+
+const like = () => {
+  const likeBtn = document.querySelectorAll('.heart-btn');
+  console.log(likeBtn);
+};
+export { domCheck, like };
