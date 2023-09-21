07d08757231d752fec9156d0435d909ef21810c3
FilbertKimasa <filbertdan67@gmail.com>
2023-09-21 09:24:17 +0300

    Change movieId to item_id in the add like function parameters

--- src/modules/movie-likes.js
+++ src/modules/movie-likes.js
@@ -1,29 +1,26 @@
 import likeUrl from './from-api.js';
 
 const domCheck = document.querySelector('.movies-counter');
-
-const addLike = async (movieId = 'movieId', id) => {
+const addLike = async (item_id = 'item_id', id) => {
   try {
-    const response = await fetch(likeUrl, {
+    await fetch(likeUrl, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ movieId, id }),
+      body: JSON.stringify({ item_id, id }),
     });
-    console.log(response);
   } catch {
     throw new Error();
   }
 };
+
 const like = () => {
   const likeBtn = document.querySelectorAll('.heart-btn');
   likeBtn.forEach((element) => {
     element.addEventListener('click', (e) => {
       const { id } = e.target.closest('.movie-container');
       addLike(parseInt(id, 10));
-
-      //   console.log(typeof parseInt(id, 10));
     });
   });
 };
