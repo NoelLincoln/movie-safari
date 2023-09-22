e9dafd5ded1a356af31b1d53cdc1bd9cec468299
FilbertKimasa <filbertdan67@gmail.com>
2023-09-22 18:58:22 +0300

    Modify movieCounter function

--- src/modules/movie-counter.js
+++ src/modules/movie-counter.js
@@ -1,5 +1,7 @@
 const movieCounter = () => {
-  document.querySelectorAll('.movie-container');
+  const movies = document.querySelectorAll('.movie-container');
+  const moviesNumber = document.querySelector('.movies-count');
+  moviesNumber.innerText = `(${movies.length})`;
 };
 
 export default movieCounter;
