7cd5772d6a613422756a126f236e5f1921903524
FilbertKimasa <filbertdan67@gmail.com>
2023-09-23 08:37:56 +0300

    Add movie-counter.test.js file

---
+++ src/modules/movie-counter.test.js
@@ -0,0 +1,14 @@
+import movieCounter from './movie-counter.js';
+
+document.body.innerHTML = `
+  <div class="movie-container"></div>
+  <div class="movie-container"></div>
+  <div class="movies-count"></div>
+`;
+
+test('movieCounter updates the movies count correctly', () => {
+  movieCounter();
+
+  const moviesNumber = document.querySelector('.movies-count');
+  expect(moviesNumber.innerText).toBe('(2)'); // Adjust the expected value as per your test case
+});
