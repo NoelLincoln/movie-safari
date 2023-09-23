99776eee92d423402c6d048f90788f10dd978e46
FilbertKimasa <filbertdan67@gmail.com>
2023-09-23 20:02:05 +0300

    Updates

--- src/modules/from-api.js
+++
@@ -1,7 +0,0 @@
-const interactionUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi';
-const appId = 'IR7MovTrVQtBQVyC6UTK';
-const likeUrl = `${interactionUrl}/apps/${appId}/likes/`;
-
-const movieUrl = 'https://api.tvmaze.com/schedule/web?date=2020-05-29';
-
-export { likeUrl, movieUrl };
--- src/modules/movie-counter.js
+++
@@ -1,7 +0,0 @@
-const movieCounter = () => {
-  const movies = document.querySelectorAll('.movie-container');
-  const moviesNumber = document.querySelector('.movies-count');
-  moviesNumber.innerText = `(${movies.length})`;
-};
-
-export default movieCounter;
